b7a85f044de024de42c1ae84f7344f42225023a5
secnonsense <scott@secnonsense.com>
2021-06-13 12:51:23 -0400

    moving forward

--- snapshit.py
+++ snapshit.py
@@ -3,39 +3,43 @@ import hashlib
 import argparse
 
 def directory_list(path):
-    dir = next(os.walk(path))[2]
-    return dir
+    directory=[]
+    for dirpath, _, files in os.walk(path):
+        for f in files:
+            directory.append(os.path.join(dirpath, f))
+    return directory
 
 def generate_hash(path,dir,snapshit="snapshit.txt"):
     with open(snapshit,'w',encoding='utf-8') as output:
         for file in dir:
             hash = hashlib.sha256()
             BLOCK_SIZE = 65536
-            print(path+file)
-            with open(path+file,'rb') as f:
+            print(file)
+            with open(file,'rb') as f:
                 for byte_block in iter(lambda: f.read(BLOCK_SIZE),b""):
                     hash.update(byte_block)
             output.write(file + "," + hash.hexdigest() + "\n")
 
-def compare_files(file,file2,nomatch=0,i=0,d=0,x=0):
+def compare_string(string1,string2):
+    if string1 == string2:
+        print(f"string {string1} matches {string2}")
+        toggle=1
+        return toggle
+
+def compare_files(file,file2):
     count=0
     with open(file, 'r') as string1_file:
             for string1 in string1_file:
                 with open(file2, 'r') as string2_file:
                     match=0
                     for string2 in string2_file:
-                        toggle=compare_string(string1.strip(),string2.strip(),nomatch,i,d,x)
+                        toggle=compare_string(string1.strip(),string2.strip())
                         if toggle==1:
                             match=1
                             count+=1
-                    if match==0 and nomatch==1:
+                    if match==0:
                         print(f"No match for {string1.strip()}") 
-                    elif match==0 and d==1:
-                        print(string1.strip())
-    if i==1 or d==1:
-        print("\n")
-    else:
-        print(f"\nTotal Matches: {count}\n")    
+ 
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -52,8 +56,18 @@ def main():
         snapshit=args.snapshit
     else:
         snapshit="./snapshit.txt"
-    dir=directory_list(args.spath)
-    generate_hash(args.spath,dir,snapshit)
+    if args.compare and not args.snapshit:
+        snapshit="./cshit.txt"
+    if args.dpath and args.compare:
+        compare_files(args.spath,args.dpath)
+    elif args.dpath and not args.compare:
+        print("You can't specify a 2nd positional argument without -c")
+        quit()
+    else:
+        dir=directory_list(args.spath)
+        generate_hash(args.spath,dir,snapshit)
+    if args.compare:
+        compare_files("./snapshit.txt","./cshit.txt")
 
 if __name__ == "__main__":
     main()
